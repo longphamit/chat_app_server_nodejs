85c89643b67dac63408295a7b3350e20ab0a7b9a
longphamit <longphamjustdoit@gmail.com>
2021-10-19 01:04:13 +0700

    update group socket

--- src/socket/GroupSocket.js
+++ src/socket/GroupSocket.js
@@ -3,9 +3,10 @@ const GROUP_MESSAGE="GROUP_MESSAGE";
 //Send message to only a particular user
 const GroupSocket=(socket)=>{
     socket.on(GROUP_MESSAGE, (message) => {
-        var receiverId = message.receiverChatID;
-        var senderId = message.senderChatID;
-        var senderName = message.senderChatName;
+        console.log(message);
+        var receiverId = message.receiverId;
+        var senderId = message.senderId;
+        var senderName = message.senderName;
         var content = message.content;
         //save message to db
         Message.create({SenderId: senderId,SenderName:senderName, ReceiverId: receiverId, Content: content}).then(result => {
